5f85520643a1c9c41352cdf43652588c1d7efe5e
DuyHuynh3010 <duyhuynh3010@gmail.com>
2025-04-22 01:40:53 -0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,52 @@
+from turtle import Screen
+from score_board import Scoreboard
+import time
+
+import score_board
+
+from snake import Snake
+from food import Food
+
+screen=Screen()
+screen.setup(width=600,height=600)
+screen.bgcolor("black")
+screen.title("Duy's Snake Game")
+screen.tracer(0)
+
+snake=Snake()
+food=Food()
+score_board=Scoreboard()
+
+screen.listen()
+screen.onkey(snake.right,"Right")
+screen.onkey(snake.left,"Left")
+screen.onkey(snake.up,"Up")
+screen.onkey(snake.down,"Down")
+
+game_status=True
+while game_status:
+    screen.update()
+    time.sleep(0.1)
+    snake.move()
+    if snake.snake_head.distance(food)<15:
+        food.refresh()
+        snake.expand()
+        score_board.clear()
+        score_board.increase_score()
+
+    if snake.snake_head.xcor()>290 or snake.snake_head.xcor()<-290 or snake.snake_head.ycor()>290 or snake.snake_head.ycor()<-290:
+        game_status = False
+        score_board.gameover()
+
+
+    for segment in snake.segments[1:]:
+        if snake.snake_head.distance(segment)<10:
+            score_board.gameover()
+            game_status=False
+
+
+
+screen.exitonclick()
+
+
+
---
+++ snake.py
@@ -0,0 +1,52 @@
+from turtle import Turtle
+from collections import deque
+class Snake:
+    def __init__(self):
+        self.segments = []
+        self.create_snake()
+        self.snake_head=self.segments[0]
+        self.direction_queue=deque()
+
+
+
+    def create_snake(self):
+        starting_position=[(0,0),(-20,0),(-40,0)]
+        for position in starting_position:
+            self.add_segments(position)
+
+    def add_segments(self,position):
+        tim = Turtle()
+        tim.shape("square")
+        tim.color("white")
+        tim.penup()
+        tim.goto(position)
+        self.segments.append(tim)
+
+    def expand(self):
+        self.add_segments(self.segments[-1].position())
+
+    def move(self):
+        if self.direction_queue:
+            new_heading=self.direction_queue.popleft()
+            self.snake_head.setheading(new_heading)
+        for seg in range(len(self.segments) - 1, 0, -1):
+            new_x = self.segments[seg - 1].xcor()
+            new_y = self.segments[seg - 1].ycor()
+            self.segments[seg].goto(new_x, new_y)
+        self.snake_head.forward(20)
+
+    def up(self):
+        if self.snake_head.heading() != 270:
+            self.direction_queue.append(90)
+
+    def down(self):
+        if self.snake_head.heading() != 90:
+            self.direction_queue.append(270)
+
+    def left(self):
+        if self.snake_head.heading() != 0:
+            self.direction_queue.append(180)
+
+    def right(self):
+        if self.snake_head.heading() != 180:
+            self.direction_queue.append(0)
